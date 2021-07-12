9036b9c314fc98ff593c04f023ca30a51b3e4066
felipedofd <felipedofd@hotmail.com>
2021-07-12 10:05:59 -0300

    status do processo

--- app/src/main/java/br/com/projetonoticias/MainActivity.java
+++ app/src/main/java/br/com/projetonoticias/MainActivity.java
@@ -2,16 +2,63 @@ package br.com.projetonoticias;
 
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
-import android.os.PersistableBundle;
+import android.util.Log;
+
 
 import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+
+import br.com.projetonoticias.util.JsonDownloadTask;
+
 
 public class MainActivity extends AppCompatActivity {
+
+    private String apiKey = "ae68700d7dad43d0bc90bda8e85caa12";
+
+
+
+
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("FELIPE", "onCreate");
+
+        new JsonDownloadTask(this)
+                .execute("https://newsapi.org/v2/everything?apiKey=ae68700d7dad43d0bc90bda8e85caa12&q=Counter Strike&language=pt");
+
     }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d("FELIPE", "onStart");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d("FELIPE", "onResume");
+    }
+
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d("FELIPE", "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d("FELIPE", "onStop");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d("FELIPE", "onDestroy");
+    }
+
+
 }
 
