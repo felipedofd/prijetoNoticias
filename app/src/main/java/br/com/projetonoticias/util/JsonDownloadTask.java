9fd7a552282046710727120a5aa7e8def4983cf1
felipedofd <felipedofd@hotmail.com>
2021-07-12 10:46:21 -0300

    Adicionando Imports

--- app/src/main/java/br/com/projetonoticias/util/JsonDownloadTask.java
+++ app/src/main/java/br/com/projetonoticias/util/JsonDownloadTask.java
@@ -1,3 +1,22 @@
 package br.com.projetonoticias.util;
+
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.os.AsyncTask;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+
 public class JsonDownloadTask extends AsyncTask<String, Void, List<Article>> {
 }
