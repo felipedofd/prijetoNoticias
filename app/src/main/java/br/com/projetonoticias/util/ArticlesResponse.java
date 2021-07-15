3d1f1d9072bec790af71c818f280a336ff24ca4c
felipedofd <felipedofd@hotmail.com>
2021-07-15 18:17:44 -0300

    Update ArticlesResponse.java

--- app/src/main/java/br/com/projetonoticias/util/ArticlesResponse.java
+++ app/src/main/java/br/com/projetonoticias/util/ArticlesResponse.java
@@ -1,7 +1,5 @@
 package br.com.projetonoticias.util;
 
-import android.widget.ScrollView;
-
 import java.util.List;
 
 public class ArticlesResponse {
@@ -10,7 +8,7 @@ public class ArticlesResponse {
     String totalResults;
     List<Article> articles;
 
-    public ArticlesResponse(String status, String totalResults, List <Article> articles){
+    public ArticlesResponse(String status, String totalResults, List<Article> articles) {
         this.status = status;
         this.totalResults = totalResults;
         this.articles = articles;
