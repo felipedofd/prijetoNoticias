b2edd2ff2661e6101fae2bb66dc1644105902ea3
felipedofd <felipedofd@hotmail.com>
2021-07-15 18:09:09 -0300

    Update MainActivity.java

--- app/src/main/java/br/com/projetonoticias/MainActivity.java
+++ app/src/main/java/br/com/projetonoticias/MainActivity.java
@@ -1,7 +1,6 @@
 package br.com.projetonoticias;
 
 import android.os.Bundle;
-import android.util.Log;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -15,53 +14,29 @@ import br.com.projetonoticias.util.ArticleAdapter;
 import br.com.projetonoticias.util.JsonDownloadTask;
 
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements JsonDownloadTask.NewsLoader {
 
     private String apiKey = "ae68700d7dad43d0bc90bda8e85caa12";
+    private RecyclerView recyclerView;
 
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Log.d("FELIPE", "onCreate");
+        recyclerView = findViewById(R.id.lista);
+
 
         JsonDownloadTask thread = new JsonDownloadTask(this);
+        thread.setNewsLoader(this);
         thread.execute("https://newsapi.org/v2/everything?apiKey=ae68700d7dad43d0bc90bda8e85caa12&q=Counter Strike&language=pt");
-        
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.d("FELIPE", "onStart");
-    }
-
-    @Override
-    protected void onResume() {
-        super.onResume();
-        Log.d("FELIPE", "onResume");
     }
 
 
     @Override
-    protected void onPause() {
-        super.onPause();
-        Log.d("FELIPE", "onPause");
+    public void onResult(List<Article> articlesResponses) {
+        recyclerView.setLayoutManager(new LinearLayoutManager(this, RecyclerView.VERTICAL, false));
+        recyclerView.setAdapter(new ArticleAdapter(articlesResponses));
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.d("FELIPE", "onStop");
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.d("FELIPE", "onDestroy");
-    }
-
-
 }
 
