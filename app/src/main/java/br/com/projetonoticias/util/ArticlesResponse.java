d73ef3230ea173ca031c938655e40a2538939a9b
felipedofd <felipedofd@hotmail.com>
2021-07-19 13:46:20 -0300

    resosta da lista, retornando os artigos

--- app/src/main/java/br/com/projetonoticias/util/ArticlesResponse.java
+++ app/src/main/java/br/com/projetonoticias/util/ArticlesResponse.java
@@ -15,5 +15,10 @@ public class ArticlesResponse {
         this.totalResults = totalResults;
         this.articles = articles;
 
+
+    }
+
+    public List<Article> getArticles() {
+        return articles;
     }
 }
